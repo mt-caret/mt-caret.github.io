fdeca42306b6c79d276f10226fe2d7efe329dc15
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2015-02-28 10:42:26 +0900

    enable generation for more than one at a time

--- th/th.js
+++ th/th.js
@@ -1,15 +1,26 @@
-function random_index(size) {
-  return Math.floor(Math.random() * size);
+function empty_list() {
+  document.getElementById("result").innerHTML = "";
 }
 
-function insert_text(str) {
-  document.getElementById("result").innerText = str;
+function add_to_list(str) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(str);
+  node.appendChild(textnode);
+  document.getElementById("result").appendChild(node);
+}
+
+function random_index(size) {
+  return Math.floor(Math.random() * size);
 }
 
 function main() {
   var names = JSON.parse(data);
   document.getElementById("generatebutton").addEventListener("click", function() {
-    insert_text(names[random_index(names.length)]);
+    var num = document.getElementById("generatenum").value;
+    empty_list();
+    for (i = 0; i < num; i++) {
+      add_to_list(names[random_index(names.length)]);
+    }
   });
 }
 
