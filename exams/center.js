77e445ae3a40f8450a4d1df2d36214c88df7605c
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2015-05-06 21:59:22 +0900

    add center exam countdown.

---
+++ exams/center.js
@@ -0,0 +1,16 @@
+center_date = new moment("2016-01-17 09:30");
+
+function update_center_date(str) {
+  document.getElementById("days_until_center").textContent = str
+}
+
+function get_days_until_center(now) {
+  return center_date.diff(now, 'days');
+}
+
+function main() {
+  var now = moment();
+  update_center_date(get_days_until_center(now));
+}
+
+document.onload = main();
