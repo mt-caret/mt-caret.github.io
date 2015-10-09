3d4de9d1e72fd23538e6a6dd3cb9cef81bc6bf67
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2015-10-10 02:48:06 +0900

    refactor generation code

--- th/th.js
+++ th/th.js
@@ -1,27 +1,13 @@
-function empty_list() {
-  document.getElementById("result").innerHTML = "";
-}
-
-function add_to_list(str) {
-  var node = document.createElement("li");
-  var textnode = document.createTextNode(str);
-  node.appendChild(textnode);
-  document.getElementById("result").appendChild(node);
-}
-
-function random_index(size) {
-  return Math.floor(Math.random() * size);
-}
-
-function main() {
+/*jslint browser: true*/
+/*global $, _, data*/
+$(document).ready(function() {
   var names = JSON.parse(data);
-  document.getElementById("generatebutton").addEventListener("click", function() {
-    var num = document.getElementById("generatenum").value;
-    empty_list();
-    for (i = 0; i < num; i++) {
-      add_to_list(names[random_index(names.length)]);
-    }
+  $("#generatebutton").on("click", function() {
+    $("#resultlist").empty();
+    $("#resultlist").append(
+      _.map(_.sample(names, $("#generatenum").val()),
+        function(name) { return "<li>" + name + "</li>"; })
+    );
   });
-}
+});
 
-document.onload = main();
