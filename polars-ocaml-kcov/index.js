1e4652e4258e92332201ed57751bf0c2ffe5fc20
Masayuki Takeda <mtakeda@oeconomia.jp>
2023-09-17 16:02:00 +0000

    add polars-ocaml kcov

---
+++ polars-ocaml-kcov/index.js
@@ -0,0 +1,9 @@
+var data = {files:[
+{"link":"inline_test_runner_polars_async.exe.5c85c66d/index.html","title":"inline_test_runner_polars_async.exe","summary_name":"inline_test_runner_polars_async.exe","covered_class":"lineNoCov","covered":"0.0","covered_lines":"0","uncovered_lines":"4364","total_lines" : "4364"},
+{"link":"inline_test_runner_polars_guide.exe.04980865/index.html","title":"inline_test_runner_polars_guide.exe","summary_name":"inline_test_runner_polars_guide.exe","covered_class":"linePartCov","covered":"25.5","covered_lines":"1115","uncovered_lines":"3249","total_lines" : "4364"},
+{"link":"inline_test_runner_polars.exe.f27ff31a/index.html","title":"inline_test_runner_polars.exe","summary_name":"inline_test_runner_polars.exe","covered_class":"lineNoCov","covered":"1.4","covered_lines":"59","uncovered_lines":"4305","total_lines" : "4364"},
+{"link":"inline_test_runner_polars_tests.exe.f2462d7d/index.html","title":"inline_test_runner_polars_tests.exe","summary_name":"inline_test_runner_polars_tests.exe","covered_class":"lineNoCov","covered":"16.8","covered_lines":"733","uncovered_lines":"3631","total_lines" : "4364"},
+], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"linePartCov","covered":"35.7","covered_lines":"1558","uncovered_lines":"2806","total_lines" : "4364"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "inline_test_runner_polars_tests.exe", "date" : "2023-09-17 15:58:55", "instrumented" : 17456, "covered" : 1907,};
---
+++ polars-ocaml-kcov/kcov-merged/index.js
@@ -0,0 +1,14 @@
+var data = {files:[
+{"link":"timezones.rs.42cb5b50.html","title":"timezones.rs","summary_name":"[...]/default/rust/target/debug/build/chrono-tz-0aa73b6cd7cecec5/out/timezones.rs","covered_class":"lineNoCov","covered":"0.7","covered_lines":"16","uncovered_lines":"2383","total_lines" : "2399"},
+{"link":"data_frame.rs.c66d23b9.html","title":"data_frame.rs","summary_name":"[...]/default/rust/polars-ocaml/src/data_frame.rs","covered_class":"lineCov","covered":"87.9","covered_lines":"305","uncovered_lines":"42","total_lines" : "347"},
+{"link":"misc.rs.541e8daa.html","title":"misc.rs","summary_name":"[...]/default/rust/polars-ocaml/src/misc.rs","covered_class":"lineCov","covered":"93.8","covered_lines":"60","uncovered_lines":"4","total_lines" : "64"},
+{"link":"series.rs.9587c14d.html","title":"series.rs","summary_name":"[...]/default/rust/polars-ocaml/src/series.rs","covered_class":"lineCov","covered":"80.9","covered_lines":"318","uncovered_lines":"75","total_lines" : "393"},
+{"link":"expr.rs.7e58d9.html","title":"expr.rs","summary_name":"[...]/default/rust/polars-ocaml/src/expr.rs","covered_class":"linePartCov","covered":"73.4","covered_lines":"430","uncovered_lines":"156","total_lines" : "586"},
+{"link":"sql_context.rs.2b5f983.html","title":"sql_context.rs","summary_name":"[...]/default/rust/polars-ocaml/src/sql_context.rs","covered_class":"linePartCov","covered":"73.7","covered_lines":"28","uncovered_lines":"10","total_lines" : "38"},
+{"link":"lib.rs.f5022ca1.html","title":"lib.rs","summary_name":"[...]/default/rust/polars-ocaml/src/lib.rs","covered_class":"lineCov","covered":"100.0","covered_lines":"1","uncovered_lines":"0","total_lines" : "1"},
+{"link":"utils.rs.e1c9cb26.html","title":"utils.rs","summary_name":"[...]/default/rust/polars-ocaml/src/utils.rs","covered_class":"lineCov","covered":"85.6","covered_lines":"225","uncovered_lines":"38","total_lines" : "263"},
+{"link":"lazy_frame.rs.fa611ace.html","title":"lazy_frame.rs","summary_name":"[...]/default/rust/polars-ocaml/src/lazy_frame.rs","covered_class":"linePartCov","covered":"64.1","covered_lines":"175","uncovered_lines":"98","total_lines" : "273"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "inline_test_runner_polars_tests.exe", "date" : "2023-09-17 15:58:55", "instrumented" : 4364, "covered" : 1558,};
+var merged_data = [];
